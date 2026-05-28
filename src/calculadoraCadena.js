6348d9f6c47e30d86efb1b08902db2c36779f261
ASalinasVe <alejandra.salinas@ucb.edu.bo>
2026-05-28 02:16:06 -0400

    feat: tdd-4 funcion para multiples numeros y separador por guion

--- src/calculadoraCadena.js
+++ src/calculadoraCadena.js
@@ -1,7 +1,7 @@
 function sumar(cadena) {
     if (cadena === "") return 0;
-    const numeros = cadena.split(",");
-    return numeros.reduce((acc, curr) => acc + parseInt(curr), 0);
+    const numeros = cadena.split(/,|\-/);
+    return numeros.reduce((acc, curr) => acc + parseInt(curr || 0), 0);
 }
 
 module.exports = { sumar };
--- src/calculadoraCadena.test.js
+++ src/calculadoraCadena.test.js
@@ -11,3 +11,8 @@ test('debería retornar el mismo número si la cadena solo tiene un número', ()
 test('debería sumar dos números separados por coma', () => {
     expect(sumar("1,2")).toBe(3);
 });
+
+test('debería sumar múltiples números separados por comas o guiones', () => {
+    expect(sumar("1-2,3")).toBe(6);
+    expect(sumar("1,2,4,8")).toBe(15);
+});
