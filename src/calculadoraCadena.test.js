c3c618580485ce88005eea90e143ffc83994c6c1
ASalinasVe <alejandra.salinas@ucb.edu.bo>
2026-05-28 02:14:07 -0400

    feat: tdd-3 funcion suma para dos numeros separados por coma

--- src/calculadoraCadena.test.js
+++ src/calculadoraCadena.test.js
@@ -7,3 +7,7 @@ test('debería retornar 0 para una cadena vacía', () => {
 test('debería retornar el mismo número si la cadena solo tiene un número', () => {
     expect(sumar("2")).toBe(2);
 });
+
+test('debería sumar dos números separados por coma', () => {
+    expect(sumar("1,2")).toBe(3);
+});
